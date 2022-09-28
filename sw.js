6da2dbf50a2f6f163dddc663c9329fcb9c4deaa5
Vontrailop <davidperez@utez.edu.mx>
2022-09-27 22:02:45 -0500

    [UPDATE] sw.js
    Añadí los estilos

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 self.addEventListener('install', (event) => {
-    console.log("SW INSTALL");
     const promiseCache = caches.open('cache-v1.1').then(cache => {
         return cache.addAll(
             [
@@ -8,7 +7,9 @@ self.addEventListener('install', (event) => {
                 '/pages/page1.html',
                 '/pages/page2.html',
                 '/pages/page3.html',
-                '/pages/page4.html'
+                '/pages/page4.html',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
             ]
 
         )
